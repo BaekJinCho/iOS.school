a7dd712c5cdeeabde37cedfed95ccde28c21d0a5
조백진 <bjjo6457@gmail.com>
2017-02-08 10:19:25 +0900

    Created Project

---
+++ ConceptProject/170208/Login/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  Login
+//
+//  Created by 조백진 on 2017. 2. 8..
+//  Copyright © 2017년 BaekJin.Cho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //Offset x = self.view.frame.size.width;
+    
+    //UIScrollView *sc = [[UIScrollView alloc] initWithFrame:CGRectMake(, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)]
+}
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
