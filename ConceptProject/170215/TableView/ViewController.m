e75a94038d51a834fc35957227faf60cf6379481
조백진 <bjjo6457@gmail.com>
2017-02-15 21:57:30 +0900

    Created TableView Project and Add mdFile Content

---
+++ ConceptProject/170215/TableView/ViewController.m
@@ -0,0 +1,94 @@
+//
+//  ViewController.m
+//  TableView
+//
+//  Created by 조백진 on 2017. 2. 14..
+//  Copyright © 2017년 BaekJin.Cho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+<UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UITableView *table = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    table.delegate = self;
+    table.dataSource = self;
+    [self.view addSubview:table];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    switch (section) {
+        case 0:
+            return 10;
+            break;
+        case 1:
+            return 5;
+            break;
+        case 2:
+            return 3;
+            break;
+        default:
+            return 0;
+            break;
+    }
+    return 100;
+}
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    
+    switch (section) {
+        case 0:
+            return @"section 1";
+            break;
+        case 1:
+            return @"section 2";
+            break;
+        case 2:
+            return @"section 3";
+            break;
+        default:
+            return @"";
+            break;
+    }
+}
+
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    
+//    return 100;
+//}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if( cell == nil ){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"]; //cell이 nil일 때만실
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"test %ld", indexPath.row];
+    
+    NSLog(@"test");
+    return cell;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ConceptProject/170215/TableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableView
+//
+//  Created by 조백진 on 2017. 2. 14..
+//  Copyright © 2017년 BaekJin.Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
