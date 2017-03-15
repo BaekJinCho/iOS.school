63fbbb186be30752156189d108c69be19aa29400
조백진 <bjjo6457@gmail.com>
2017-03-15 09:55:04 +0900

    Add GCD SourceCode

--- ConceptProject/170313(Thread)/Thread/ViewController.m
+++ ConceptProject/170313(Thread)/Thread/ViewController.m
@@ -13,9 +13,14 @@
 @property (weak, nonatomic) IBOutlet UIButton *changeBtn;
 @property (weak, nonatomic) IBOutlet UIButton *changeBtn2;
 
-@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+@property (nonatomic) IBOutlet UILabel *numberLabel;
 @property NSThread *thread;
+
+
 @property dispatch_queue_t queue;
+@property (nonatomic) NSInteger i;
+
+@property dispatch_source_t mainTimer;
 @end
 
 @implementation ViewController
@@ -24,6 +29,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    /*********************dispatch 만들어보기******************************/
     dispatch_async(dispatch_get_main_queue(), ^{
         NSLog(@"2");
     });
@@ -48,37 +54,62 @@
 
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
 - (IBAction)changeNumber:(UIButton *)sender {
-    self.thread = [[NSThread alloc] initWithTarget:self selector:@selector(startThread) object:nil];
-    [self.thread start];
-//    self.queue = dispatch_queue_create("queue", DISPATCH_QUEUE_CONCURRENT);
     
-    [self.changeBtn setTitle:@"시작" forState:UIControlStateNormal];
+    /*********************버튼 click하면 label이 바뀌는 것 -> NSThread 만들기******************************/
+//    self.thread = [[NSThread alloc] initWithTarget:self selector:@selector(startThread) object:nil];
+//    [self.thread start];
+//    [self.changeBtn setTitle:@"시작" forState:UIControlStateNormal];
+    [self startThread];
 }
 
 - (IBAction)changeBtn:(UIButton *)sender {
     
-    [self.thread cancel];
+//    [self.thread cancel]; NSThread 정지
+
+    dispatch_suspend(self.mainTimer); //dispatch 정지
 }
 
 
 - (void)startThread{
-    
-    NSInteger i = 0;
-    
-    while (!self.thread.isCancelled) {
+    /*********************스탑워치 만들기!******************************/
+    if (!self.mainTimer) {
+        self.mainTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
         
-        sleep(1);
-        [self performSelectorOnMainThread:@selector(changeText:) withObject:[NSString stringWithFormat:@"%ld",i] waitUntilDone:YES];
-        i += 1;
+        dispatch_source_set_timer(self.mainTimer, DISPATCH_TIME_NOW, NSEC_PER_SEC * 1,NSEC_PER_SEC * 1);
+        dispatch_source_set_event_handler(self.mainTimer, ^{
+            [self.numberLabel setText:[NSString stringWithFormat:@"%ld", self.i]];
+            NSLog(@"%ld", self.i);
+            self.i += 1;
+        });
     }
+    
+    dispatch_resume(self.mainTimer); //dispatch 정지
+    /*********************버튼 click하면 label이 바뀌는 것 -> dispatch 만들기******************************/
+//    self.queue = dispatch_queue_create("queue name", DISPATCH_QUEUE_CONCURRENT);
+//    dispatch_async(self.queue, ^{
+//        for (self.i = 0; self.i<100; self.i++) {
+//            dispatch_sync(dispatch_get_main_queue(), ^{
+//                [self.numberLabel setText:[NSString stringWithFormat:@"%ld", self.i]];
+//            });
+            
+//        }
+//    });
+    
+    
+    
+    /*********************버튼 click하면 label이 바뀌는 것 -> NSThread 만들기******************************/
+//    NSInteger i = 0;
+//    while (!self.thread.isCancelled) {
+//        sleep(1);
+//        [self performSelectorOnMainThread:@selector(changeText:) withObject:[NSString stringWithFormat:@"%ld",i] waitUntilDone:YES];
+//        i += 1;
+//    }
     
 //    for (NSInteger i=0; i<50; i++) {
 //        sleep(1);
